04dc9ae5ae3cbe9fd9a15e5a9f22ff224e80df92
Agnieszka Matyja <agmatyja@gmail.com>
2019-03-21 22:17:10 +0100

    finds countries correctly

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+	var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = document.getElementById('countries');
+
+document.getElementById('search').addEventListener('click', searchCountries);
+
+function searchCountries() {
+    var countryName = document.getElementById('country-name').value;
+    if(!countryName.length) countryName = 'Poland';
+    fetch(url + countryName)
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(showCountriesList);
+}
+function showCountriesList(resp) {
+  countriesList.innerHTML = 'countryName';
+  resp.forEach(function(item){
+    var liEl = document.createElement('li');
+    liEl.innerText = item.name;
+    countriesList.appendChild(liEl);
+});
+
+}});
